45735654962a6ce10bbdab1a43d3be07aa435a36
Bipin Joshi <bussyperson88550@gmail.com>
2026-08-29 02:11:51 +0530

    LC-destination-city [Runtime: N/A; Memory: N/A]

---
+++ string/destination-city.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        int n = paths.size();
+        int m = paths[0].size();
+
+        unordered_set<string>st;
+
+        for(int i = 0 ; i < n ; i++){
+            st.insert(paths[i][0]);
+        }
+
+        string ans = "";
+        for(int i = 0 ; i < n ; i++){
+            if(st.find(paths[i][1]) == st.end()){
+                ans = paths[i][1];
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
