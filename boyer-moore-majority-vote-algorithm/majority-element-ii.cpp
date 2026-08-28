cd0be40c86ecb8e5f41c1d7be5a754c4a6b5de80
Bipin Joshi <bussyperson88550@gmail.com>
2026-08-28 23:50:47 +0530

    Synchronize existing submission history (5/6)

---
+++ boyer-moore-majority-vote-algorithm/majority-element-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& arr) {
+        int cnt1=0;
+        int cnt2 = 0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
+        int n = arr.size();
+        
+        for(int i = 0 ; i < n ; i++){
+            if(cnt1 == 0 && el2 != arr[i]){
+                cnt1 = 1;
+                el1 = arr[i];
+            }
+            
+            else if(cnt2 == 0 && el1 != arr[i]){
+                cnt2 = 1;
+                el2 = arr[i];
+            }
+            
+            else if(arr[i] == el1){
+                cnt1++;
+            }
+            
+            else if(arr[i] == el2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        //verify the candidates
+        cnt1 = 0; 
+        cnt2 = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i]==el1){
+                cnt1++;
+            }
+            else if(arr[i]==el2){
+                cnt2++;
+            }
+        }
+        
+        vector<int>ans;
+        if(cnt1>n/3){
+            ans.push_back(el1);
+        }
+        if(cnt2>n/3){
+            ans.push_back(el2);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
