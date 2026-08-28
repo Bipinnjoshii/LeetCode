d2edc7cd5c210d436ead242f42e3bde60a5d9577
Bipin Joshi <bussyperson88550@gmail.com>
2026-08-29 00:46:25 +0530

    LC-count-of-matches-in-tournament [Runtime: N/A; Memory: N/A]

--- simulation/count-of-matches-in-tournament.cpp
+++ simulation/count-of-matches-in-tournament.cpp
@@ -5,3 +5,29 @@ public:
         return n-1;
     }
 };
+
+
+/*class Solution {
+public:
+    int numberOfMatches(int n) {
+        int cnt = 0;
+        int num = n;
+        int matches = 0;
+        int teams = n;
+        while(num>1){
+            if(num % 2 == 0){
+                matches = num/2;
+                teams = num/2;
+                cnt += matches;
+                num = num/2;
+            }
+            else{
+                matches = (num-1)/2;
+                teams = (num-1)/2+1;
+                cnt += matches;
+                num = num/2 +1;
+            }
+        }
+        return cnt;
+    }
+};  */
