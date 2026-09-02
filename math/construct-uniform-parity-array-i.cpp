220a2ae8261ee28ffa0c353eafe4c2669ece9305
Bipin Joshi <bussyperson88550@gmail.com>
2026-09-02 23:04:01 +0530

    LC-construct-uniform-parity-array-i [Runtime: N/A; Memory: N/A]

--- math/construct-uniform-parity-array-i.cpp
+++ math/construct-uniform-parity-array-i.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int n = nums1.size();
-        vector<int>nums2(n);
+
         bool flag = true;
-        int idx = -1;
+        
         int oddelement = 0;
 
         for(int i = 0; i < n ; i++){
@@ -20,18 +20,15 @@ public:
         }
         
         for(int i = 0 ; i < n ; i++){
-            if(nums1[i] % 2 != 0){
-                nums2[i] = nums1[i];
-            }
-            else{
-                nums2[i] = nums1[i] - oddelement;
+            if(nums1[i] % 2 == 0){
+                nums1[i] = nums1[i] - oddelement;
             }
         }
         
         bool even = false;
 
         for(int i = 0 ; i < n ; i++){
-            if(nums2[i] % 2 == 0){
+            if(nums1[i] % 2 == 0){
                 even = true;
                 break;
             }
