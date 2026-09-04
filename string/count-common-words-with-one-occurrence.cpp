cb0b549a857ba3ad3654e9e875e2cff0a0b7ba93
Bipin Joshi <bussyperson88550@gmail.com>
2026-09-04 22:09:52 +0530

    LC-count-common-words-with-one-occurrence [Runtime: N/A; Memory: N/A]

--- string/count-common-words-with-one-occurrence.cpp
+++ string/count-common-words-with-one-occurrence.cpp
@@ -5,23 +5,22 @@ public:
         int m = words2.size();
         int cnt = 0;
 
-        unordered_map<string,int>mpp;
+        unordered_map<string,int>mpp1,mpp2;
 
         for(int i = 0 ; i < n ; i++){
-            mpp[words1[i]]++;
+            mpp1[words1[i]]++;
         }
 
-        for(int i = 0 ; i < m ; i++ ){
-            mpp[words2[i]]++;
+        for(int i = 0 ; i < m ; i++){
+            mpp2[words2[i]]++;
         }
 
-        for(auto it:mpp){
-            int freq = it.second;
-
-            if(freq == 2){
+        for(int i = 0 ; i < m ; i++){
+            if(mpp1[words2[i]] == 1 && mpp2[words2[i]] == 1){
                 cnt++;
             }
         }
+        
 
         return cnt;
     }
