4b1e76209d219df07170e8581218ddd5c964e2a0
Bipin Joshi <bussyperson88550@gmail.com>
2026-09-15 15:51:55 +0530

    LC-rectangle-overlap [Runtime: N/A; Memory: N/A]

--- math/rectangle-overlap.cpp
+++ math/rectangle-overlap.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        
-        if(rec1[0] < rec2[0]){
-            if(rec1[2] > rec2[0] && rec1[1] < rec2[3]){
-                return true;
-            }
-        }
-        else if(rec1[0] > rec2[0]){
-            if(rec2[2] > rec1[0] && rec2[3] > rec1[1]){
-                return true;
-            }
-        }
-        else if(rec1[0] == rec2[0]){
-            if(rec2[3] >= rec1[3]){
-                return true;
-            }
+        bool xOverlap;
+        if (rec1[0] < rec2[2] && rec2[0] < rec1[2]) {
+            xOverlap = true;
+        } else {
+            xOverlap = false;
         }
 
-        return false;
+        bool yOverlap;
+        if (rec1[1] < rec2[3] && rec2[1] < rec1[3]) {
+            yOverlap = true;
+        } else {
+            yOverlap = false;
+        }
+
+        if (xOverlap && yOverlap) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
