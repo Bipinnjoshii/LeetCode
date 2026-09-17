0a35b0bb4cdb1966bdba2218211dc0cc993cdb35
Bipin Joshi <bussyperson88550@gmail.com>
2026-09-18 00:17:00 +0530

    LC-pass-the-pillow [Runtime: N/A; Memory: N/A]

--- math/pass-the-pillow.cpp
+++ math/pass-the-pillow.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
 
-        int pass = (time+1) % n;
+        int pass = time + 1;
 
-        if(pass == 0){
-            return n;
+        if(pass > n){
+            return(n - (pass - n));
         }
         
-        int ans = n - pass;
-        return ans;
+        return pass;
     }
 };
